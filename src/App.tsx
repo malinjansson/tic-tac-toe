e4f97624333bba0a8ba78e0e0d8304e2eb1f84d6
Malin Jansson <maalin.jaansson@gmail.com>
2025-05-21 16:20:11 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { TicTacToe } from './components/TicTacToe'
+
+function App() {
+  return (
+    <>
+    <h1>Let's play TicTacToe!</h1>
+    <TicTacToe />
+    </>
+  )
+}
+
+export default App
